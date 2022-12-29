4265fd151dab1c38e8b17921b06a7eb9f5dc3fb8
Anandakumar A <111185338+anandakumar0962@users.noreply.github.com>
2022-12-29 16:12:02 +0530

    Delete dailycode 12.py

--- dailycode 12.py
+++
@@ -1,6 +0,0 @@
-input_list = list(map(int, input().split(" ")))
-result = []
-for i in range(len(input_list)-2):
-    a = max(input_list[i], input_list[i+1], input_list[i+2])
-    result.append(a)
-print(result)
